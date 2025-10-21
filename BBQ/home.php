643a3e2d547d984d342722254c79f1e7ef6514c4
1021-afk <6731501021@lamduan.mfu.ac.th>
2025-10-21 22:19:25 +0700

    Add files via upload

---
+++ BBQ/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Big Bro sQuad.com</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+        <link rel="stylesheet" type ="text/css" href="css/CSS home.css">
+    </head>
+    <body>
+        <nav>
+            <div class="nav-container">
+            <a href="img/dbell2.png">
+                <img src="img/dbell2.png" class="logonav">
+            </a>
+            <div class="nav-profile">
+                <p class="nav-profile-name">Bug</p>
+                <div class="nav-profile-cart">
+                    <i class="fas fa-cart-shopping"></i>
+                </div>
+            </div>
+        </div>
+        </nav>
+    </body>
+</html>
